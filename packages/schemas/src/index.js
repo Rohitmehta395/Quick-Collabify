ad49bdcede3f40866081ece669ab90501a7fabf1
Rohit Mehta <rohitmehtaddn@gmail.com>
2026-07-11 22:17:07 +0530

    chore: scaffold packages/schemas package

---
+++ packages/schemas/src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @workspace/schemas
+ *
+ * Central registry for all Domain Zod Schemas.
+ * (e.g., User Schema, Document Schema, Cursor Schema).
+ *
+ * We will export them here as named exports when they are implemented in future phases.
+ */
+
+export {};
